9e83a28c00ab0b8464c8b10799a91fff593f62d0
Bilal Madi <bilal@bilalmadi.com>
2026-05-14 20:09:54 -0400

    Add sitemap and SEO stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	// Enable React to support React JSX components.
-	integrations: [react()],
+	integrations: [react(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	site: 'https://bilalmadi.com',
 });
